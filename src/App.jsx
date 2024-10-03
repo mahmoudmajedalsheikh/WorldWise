2b18c727f0d72ebcdd44abfab34e2cde9a8aefca
Eng Mahmoud Majed <mahmoudmajed404@gmail.com>
2024-10-03 08:13:12 +0300

    End of section 19 for this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,71 @@
-import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
-import Homepage from './pages/Homepage';
-import Pricing from './pages/Pricing';
-import Product from './pages/Product';
-import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
-import Login from "./pages/Login";
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
-import City from './components/City';
-import Form from './components/Form';
-import {CitiesProvider} from './contexts/CitiesContext';
-import {AuthProvider} from './contexts/FakeAuthContext';
-import ProtectedRoute from './pages/ProtectedRoute'
+import City from "./components/City";
+import Form from "./components/Form";
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
+import SpinnerFullPage from './components/SpinnerFullPage'
+// ================ pages
+// import Homepage from './pages/Homepage';
+// import Pricing from './pages/Pricing';
+// import Product from './pages/Product';
+// import PageNotFound from "./pages/PageNotFound";
+// import AppLayout from "./pages/AppLayout";
+// import Login from "./pages/Login";
+
 // import  from ''
 
+// dist/index.html                   0.51 kB │ gzip:   0.33 kB
+// dist/assets/index-D51nla2L.css   29.89 kB │ gzip:   5.05 kB
+// dist/assets/index-CiZBM71M.js   508.94 kB │ gzip: 148.72 kB
 
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Product = lazy(() => import("./pages/Product"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const Login = lazy(() => import("./pages/Login"));
 
 function App() {
+  return (
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Suspense fallback={<SpinnerFullPage />}>
+          <Routes>
+            <Route path="/" element={<Homepage />}></Route>
+            {/*or <Route index element={<Homepage/>}></Route> */}
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="product" element={<Product />} />
+            <Route path="*" element={<PageNotFound />} />
 
+            <Route
+              path="/app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              {/* Nested Routes */}
+              {/* index Route default  child Route*/}
+              <Route index element={<Navigate replace to="cities" />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
 
-return(
-
-  <AuthProvider>
-  <CitiesProvider>
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Homepage/>}></Route>
-    {/*or <Route index element={<Homepage/>}></Route> */}
-    <Route path="pricing" element={<Pricing/>}/>
-    <Route path="product" element={<Product/>}/>
-    <Route path="*" element={<PageNotFound/>}/>
-
-    <Route path="/app" element={
-        <ProtectedRoute>
-          <AppLayout/> 
-        </ProtectedRoute>
-      }>
-      {/* Nested Routes */}
-      {/* index Route default  child Route*/}
-      <Route index element={<Navigate replace to='cities' />} />
-      <Route path="cities" element={<CityList/>}/>
-      <Route path="cities/:id" element={<City />} />
-      <Route path="countries" element={<CountryList/>} />
-      <Route path="form" element={<Form />} />
-    </Route>
-
-    <Route path="login" element={<Login/> }/>
-
-  </Routes>
-
-  </BrowserRouter>
-  </CitiesProvider>
-  </AuthProvider>
-
-)
+            <Route path="login" element={<Login />} />
+          </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
